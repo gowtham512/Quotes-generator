3fedc8e7e33b30d59536e7cba6a8a6533ad34b44
Pateel Sai Gowtham Reddy <113625678+gowtham512@users.noreply.github.com>
2023-05-24 19:07:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
+
+const url = "https://api.quotable.io/random";
+
+let getQuote = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      quote.innerText = item.content;
+      author.innerText = item.author;
+      date.innerText=item.dateAdded
+    });
+};
+
+window.addEventListener("load", getQuote);
+btn.addEventListener("click", getQuote);
